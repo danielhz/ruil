9b871ce9820c3bcddb58e00824becb5d033ee8ef
Daniel Hernández <daniel@degu.cl>
2011-01-20 09:19:18 -0300

    JSONTemplate test

---
+++ test/json_template_test.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'ruil/json_template'
+require 'json'
+
+describe Ruil::JSONTemplate do
+  before(:all) do
+    @template = Ruil::JSONTemplate.new
+  end
+
+  it 'should have a JSON mime type' do
+    @template.media_type.should == 'application/json'
+  end
+
+  it 'should answer to a "json" key' do
+    @template.key.should == 'json'
+  end
+
+  it 'should render json files' do
+    data = { :r => (1...10).map { |x| rand } }
+    rendered = JSON.parse(@template.render(data))
+    rendered['r'].each_index do |i|
+      rendered['r'][i].to_s.should == data[:r][i].to_s
+    end
+  end
+end
