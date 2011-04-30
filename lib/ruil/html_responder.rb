7b3887db258cb85be1555d1525730bda7bcfcc56
Daniel Hernández <daniel@degu.cl>
2011-04-30 18:05:57 -0300

    Passing tests for HTMLResponder

--- lib/ruil/html_responder.rb
+++ lib/ruil/html_responder.rb
@@ -3,8 +3,8 @@ require 'tenjin'
 
 module Ruil
 
-  # {Ruil::emplate} objects implement receive requests and respond
-  # with {Rack::Response} where bodies are HTML documents and the media
+  # {Ruil::HTMLResponder} objects implement receive requests and respond
+  # with Rack::Response where bodies are HTML documents and the media
   # type is "text/html" or "application/xhtml+xml".
   class HTMLResponder
 
@@ -22,16 +22,11 @@ module Ruil
     # user agent and preferences.
     def initialize(file_prefix, layout = nil)
       @templates = []
-      Dir[file_prefix + ".*.*.*"].each do |file|
-        a   = File.basename(@file).split('.')
-        key = a[1].to_sym
-        engine = case a[2]
-                 when "tenjin"
-                   require "tenjin"
-                   Tenjin::Engine.new({:layout => layout})
-                 else
-                   raise "Template engine unknown #{a[2]}"
-                 end
+      Dir[file_prefix + ".*.*.*"].select{ |f| ! (/\.cache$/ === f) }.each do |file|
+        a = File.basename(file).split('.')
+        # Mode
+        mode = a[1].to_sym
+        # Media type
         media_type = case a[3]
                      when "html"
                        "text/html"
@@ -40,10 +35,20 @@ module Ruil
                      else
                        raise "Media type unknow for #{a[3]}"
                      end
-        @templates[key] = {
-          :engine => engine,
-          :file => file,
-          :media_type => media_type
+        # Add template
+        engine = case a[2]
+                 when "tenjin"
+                   require "tenjin"
+                   Tenjin::Engine.new({:layout => layout})
+                 else
+                   raise "Template engine unknown #{a[2]}"
+                 end
+        @templates << {
+          :mode       => mode,
+          :engine     => engine,
+          :file       => file,
+          :media_type => media_type,
+          :suffix     => a[3].to_sym
         }
       end
     end
@@ -54,12 +59,21 @@ module Ruil
     # @param [Ruil::Request] request the request to respond.
     # @return [Rack::Response] the response.
     def call(request)
-      key = request.key
-      if @templates.include[key]
-        template = @templates[key][:engine]
-        tempalte.render(template[:file], request.generated_data)
+      case request.rack_request.path_info
+      when /.html$/
+        suffix = :html
+      when /.xhtml$/
+        suffix = :xhtml
       else
-        raise "Unknown key #{key}"
+        raise "unknown media_type #{request.rack_request.path_info}"
+      end
+      template = @templates.select{ |t| t[:suffix] == suffix }.map.first
+
+      unless template.nil?
+        body = template[:engine].render(template[:file], request.generated_data)
+        Rack::Response.new(body, 200, {'Content-Type' => template[:media_type]})
+      else
+        raise "Not found"
       end
     end
 
---
+++ spec/html_responder_spec.rb
@@ -0,0 +1,60 @@
+require 'rubygems'
+require 'ruil'
+require 'json'
+require 'rspec'
+
+describe Ruil::HTMLResponder do
+
+  before(:all) do
+    # Define templates and requests
+    @templates = []
+    request = Ruil::Request.new(Rack::Request.new({}))
+    request.rack_request.path_info = 'ruil.html'
+    request.generated_data = {:v => 'HTML'}
+    @templates << ['/tmp/ruil.desktop.tenjin.html', '<p>#{@v}</p>', request]
+    request = Ruil::Request.new(Rack::Request.new({}))
+    request.rack_request.path_info = 'ruil.xhtml'
+    request.generated_data = {:v => 'XHTML'}
+    @templates << ['/tmp/ruil.desktop.tenjin.xhtml', '<p>#{@v}</p>', request]
+    @templates.each do |t|
+      template = File.new(t[0], 'w')
+      template << t[1]
+      template.close
+    end
+    # @layout = '/tmp/layout.'
+    # File.new(t[0], 'w')
+    # @layo << t[1]
+    # template.close
+    # Define responders
+    @responder1 = Ruil::HTMLResponder.new('/tmp/ruil')
+    # @responder2 = Ruil::HTMLResponder.new('/tmp/ruil', @layout)
+  end
+
+  after(:all) do
+    # TODO: Remove template files
+  end
+
+  it 'should respond files without layout' do
+    @templates.each do |template|
+      response = @responder1.call(template[2])
+      response.status.should == 200
+      response.body.should == ["<p>#{template[2].generated_data[:v]}</p>"]
+      case template[2].rack_request.path_info
+      when /.html$/
+        response.header['Content-Type'].should == 'text/html'
+      when /.xhtml$/
+        response.header['Content-Type'].should == 'application/xhtml+xml'
+      else
+        raise
+      end
+    end
+  end
+
+  # it 'should respond using a html file with layout' do
+  #   response = @responder1.call(@request2)
+  #   response.status.should == 200
+  #   response.body.should == ['<html><body><p>HTML: hello world<p></body></html>']
+  #   response.header['Content-Type'].should == 'text/html'
+  # end
+
+end
