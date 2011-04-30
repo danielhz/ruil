cba99f045da48ec7750aa14238983d8d313c61fa
Daniel Hernández <daniel@degu.cl>
2011-04-30 13:57:58 -0300

    Improving documentation

--- lib/ruil/path_info_parser.rb
+++ lib/ruil/path_info_parser.rb
@@ -1,7 +1,22 @@
 module Ruil
 
+  # Each instance of {Ruil::PathInfoParser} matches path info strings
+  # with a pattern and return variables matching it if
+  # the pattern is matched. Else, it returns false.
+  #
+  # === Usage
+  #
+  #    parser = PathInfoParser.new('/foo/:type/:id')
+  #    parser === '/foo/bar/1'      # => {:type => 'bar', :id => '1'}
+  #    parser === '/foo/bar/3.js'   # => {:type => 'bar', :id => '3'}
+  #    parser === '/foo'            # => false
+  #    parser === '/bar'            # => false
+  #
   class PathInfoParser
 
+    # Initialize a new parser
+    # 
+    # @param pattern[String] the pattern to match
     def initialize(pattern)
       @pattern = pattern.split('/').map do |s|
        ( s[0,1] == ':' ) ? eval(s) : s
@@ -9,6 +24,11 @@ module Ruil
       @pattern = ['', ''] if @pattern.empty?
     end
 
+    # Match a path info.
+    #
+    # @param path_info[String] the path info to match.
+    # @return [Hash,false] a hash with variables matched with the
+    #   pattern or false if the path info doesn't match the pattern.
     def ===(path_info)
       s = path_info.split('/')
       s = ['', ''] if s.empty?
