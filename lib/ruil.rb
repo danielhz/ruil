b2bab4962628012f0f448c9e3b1947b46eb0b932
Daniel Hernández <daniel@degu.cl>
2011-05-01 12:05:06 -0300

    Passing all tests
    Now you can run all test with "rake spec" and build the coverage
    documentation with "rake rcov".

--- lib/ruil.rb
+++ lib/ruil.rb
@@ -1,14 +1,5 @@
 require 'rack'
 
-# Dir[File.dirname(File.expand_path(__FILE__)) + '/**/*.rb'].each do |f|
-#   require f
-# end
-
-require 'ruil/path_info_parser.rb'
-require 'ruil/request.rb'
-require 'ruil/json_responder.rb'
-require 'ruil/html_responder.rb'
-require 'ruil/redirector.rb'
-require 'ruil/register.rb'
-require 'ruil/resource.rb'
-require 'ruil/static_resource.rb'
+Dir[File.dirname(File.expand_path(__FILE__)) + '/**/*.rb'].each do |f|
+  require f
+end
--- lib/ruil/session_widget.rb
+++ lib/ruil/session_widget.rb
@@ -1,52 +1,43 @@
-module Ruil
+# module Ruil
 
-  class SessionWidget < Ruil::Widget
+#   class SessionWidget < Ruil::Widget
 
-    # Initialize a new RuilSessionWidget
-    def initialize(options = {})
-      if Hash === options
-        # Set @templates_dir
-        @templates_dir = options[:templates_dir]
-        if @templates_dir.nil?
-          if defined?(APPLICATION_PATH).nil?
-            raise "APPLICATION_PATH is undefinded"
-          else
-            @templates_dir = File.join(APPLICATION_PATH, "dynamic", "templates", "session_widget")
-          end
-        end
-        # Set @path_prefix
-        @path_prefix = options[:path_prefix] || "/session_widget/"
-        # Set @logout_redirects_to
-        @logout_redirects_to = options[:logout_redirects_to] || "/front.html"
-        # Set @users_collection
-        @users_collection = options[:users_collection]
-        if @users_collection.nil?
-          if defined?(DB).nil?
-            raise "DB undefined"
-          else
-            @users_collection = DB['users']
-          end
-        end
-      end
-      setup_resources
-    end
+#     # Initialize a new RuilSessionWidget
+#     def initialize(options = {})
+#       if Hash === options
+#         # Set @path_prefix
+#         @path_prefix = options[:path_prefix] || "/session_widget/"
+#         # Set @logout_redirects_to
+#         @logout_redirects_to = options[:logout_redirects_to] || "/front.html"
+#         # Set @users_collection
+#         @users_collection = options[:users_collection]
+#         if @users_collection.nil?
+#           if defined?(DB).nil?
+#             raise "DB undefined"
+#           else
+#             @users_collection = DB['users']
+#           end
+#         end
+#       end
+#       super
+#     end
 
-    def setup_resources
-      render "stylesheet", "stylesheet"
-      render "script", "script"
-      redirect "logout", @logout_redirects_to do
-        # Clean session
-      end
-      render "login", "login" do
-        # Authenticate
-      end
-      render "request", "request" do
-      end
-      render "check", "check" do
-        # Check code
-      end
-      render "update", "update" do
-      end
-    end
+#     def setup_actions
+#       render "stylesheet", "stylesheet"
+#       render "script", "script"
+#       redirect "logout", @logout_redirects_to do
+#         # Clean session
+#       end
+#       render "login", "login" do
+#         # Authenticate
+#       end
+#       render "request", "request" do
+#       end
+#       render "check", "check" do
+#         # Check code
+#       end
+#       render "update", "update" do
+#       end
+#     end
 
-  end
+#   end
