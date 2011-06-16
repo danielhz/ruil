3286d52a9837b2899b176c7f5f719517102ec18f
Daniel Hernández <daniel@degu.cl>
2011-06-16 10:38:59 -0400

    Not found responder

---
+++ lib/ruil/not_found_responder.rb
@@ -0,0 +1,20 @@
+
+module Ruil
+
+  # {Ruil::NotFoundResponder} objects respond with a not found message.
+  class NotFoundResponder
+
+    # Creates a not-found response to a request.
+    #
+    # @param [Ruil::Request] request the request to respond.
+    # @return [Rack::Response] the response.
+    def call(request)
+      method = request.rack_request.method
+      url    = request.rack_request.url
+      body   = "Resource not found: #{method} #{url}"
+      Rack::Response.new(body, 200, {'Content-Type' => 'text/plain'})
+    end
+
+  end
+
+end
